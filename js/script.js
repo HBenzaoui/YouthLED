4a25ec202941896209f91a84d83fdcc9ad240abe
Hamza Benzaoui <hamzapdd@gmail.com>
2019-07-22 10:49:38 +0100

    add comment for hiding fuction

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 |   |   |   |   Preloader   |   |   |   |               
 ==========================================*/
 
+//This is to make sure the whole side is loaded
 $(window).on('load', function() {
   $('#status').fadeOut();
   $('#preloader')
