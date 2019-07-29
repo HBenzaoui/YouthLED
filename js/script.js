7d6d9566ac0c136465fad6b8bef876f2ac19047d
Hamza Benzaoui <hamzapdd@gmail.com>
2019-07-29 15:13:13 +0100

    finsih add jquery slider

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ $(window).on('load', function() {
     .delay(350)
     .fadeOut('slow');
 });
+
+/*==========================================
+|   |   |   |   Team   |   |   |   |               
+==========================================*/
+$(function() {
+  $('#team-memebers').owlCarousel({
+    items: 2,
+    // margin: 15,
+    // center: true,
+    // padding: 10,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>'
+    ]
+  });
+});
