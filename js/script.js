a947099e5a3c1b2617f9ca90c728d7fc196a1f9b
Hamza Benzaoui <hamzapdd@gmail.com>
2019-07-31 01:01:49 +0100

    waypoints example

--- js/script.js
+++ js/script.js
@@ -46,3 +46,10 @@ $(function() {
     );
   });
 });
+
+var waypoint = new Waypoint({
+  element: document.getElementById('basic-waypoint'),
+  handler: function() {
+    notify('Basic waypoint triggered');
+  }
+});
